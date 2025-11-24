38eba494e2d575fae99a8da04a5f86fb62d294a4
Lovanlong1210 <161397464+Lovanlong1210@users.noreply.github.com>
2025-11-24 15:56:17 +0700

    Delete BTL directory

--- BTL/includes/auth_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-// /includes/auth_check.php
-// Đảm bảo session đã bắt đầu từ db.php
-if (!isset($_SESSION['user_id'])) {
-    header("Location: /pet_care_app/login.php");
-    exit;
-}
-?>
--- BTL/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// /logout.php
-session_start();
-session_unset();
-session_destroy();
-header("Location: /BTL/login.php"); // Chuyển hướng về trang đăng nhập
-exit;
-?>
